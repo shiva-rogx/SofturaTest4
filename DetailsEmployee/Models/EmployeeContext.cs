c1d0797b4fc5ab4ce659cbe6a30ae471bc463fd2
shiva-rogx <65455314+shiva-rogx@users.noreply.github.com>
2021-05-22 12:58:45 +0530

    Add files via upload

---
+++ DetailsEmployee/Models/EmployeeContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DetailsEmployee.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee() { Id = 1, Name = "Shiva", Age = 22, Qualification = "Graduate", IsEmployeed = "Yes", NoticePeriod = "2 Months", CurrentCTC = 50000 });
+        }
+    }
+}
